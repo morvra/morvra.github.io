eebf0457d84388bad612f0a9b39e95908ba53279
morvra <137384827+morvra@users.noreply.github.com>
2024-06-10 00:06:31 +0900

    Update piece.js

--- piece.js
+++ piece.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const jsonPath = './piece.json';
+    const jsonPath = './data.json';
     const articleContainer = document.getElementById('article-container');
     const navigationContainer = document.getElementById('navigation-container');
 
@@ -10,18 +10,21 @@ document.addEventListener('DOMContentLoaded', function() {
     fetch(jsonPath)
         .then(response => response.json())
         .then(data => {
+            // piecesを取得
+            const pieces = data.pieces;
+
             // 日付の新しい順にソート
-            data.sort((a, b) => new Date(b.date) - new Date(a.date));
+            pieces.sort((a, b) => new Date(b.date) - new Date(a.date));
 
             // 1ページあたりの記事数と記事の開始・終了インデックスを計算
             const itemsPerPage = 20;
             const startIndex = (page - 1) * itemsPerPage;
             const endIndex = startIndex + itemsPerPage;
-            const totalPages = Math.ceil(data.length / itemsPerPage);
+            const totalPages = Math.ceil(pieces.length / itemsPerPage);
 
             // 記事を表示
-            for (let i = startIndex; i < endIndex && i < data.length; i++) {
-                const { id, title, date, body } = data[i];
+            for (let i = startIndex; i < endIndex && i < pieces.length; i++) {
+                const { id, title, date, body } = pieces[i];
 
                 // 記事のタイトル、日付、本文を表示するdiv要素を作成
                 const articleDiv = document.createElement('div');
@@ -40,10 +43,20 @@ document.addEventListener('DOMContentLoaded', function() {
             if (totalPages > 1) {
                 const navigationDiv = document.createElement('div');
                 navigationDiv.classList.add('navigation');
-                navigationDiv.innerHTML = `
-                    <a href="./piece.html?page=${page - 1}" class="prev" ${page === 1 ? 'style="display: none;"' : ''}>Prev</a>
-                    <a href="./piece.html?page=${page + 1}" class="next" ${page === totalPages ? 'style="display: none;"' : ''}>Next</a>
-                `;
+                if (page > 1) {
+                    const prevLink = document.createElement('a');
+                    prevLink.href = `./piece?page=${page - 1}`;
+                    prevLink.classList.add('prev');
+                    prevLink.textContent = 'Prev';
+                    navigationDiv.appendChild(prevLink);
+                }
+                if (page < totalPages) {
+                    const nextLink = document.createElement('a');
+                    nextLink.href = `./piece?page=${page + 1}`;
+                    nextLink.classList.add('next');
+                    nextLink.textContent = 'Next';
+                    navigationDiv.appendChild(nextLink);
+                }
                 navigationContainer.appendChild(navigationDiv);
             }
         })
